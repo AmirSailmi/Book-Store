c686b82ea2bd0fac5340e3df2a457aebfc5c1a69
Amir Salimi <amirhosein.srudsari@gmail.com>
2022-07-04 12:24:48 +0430

    all files

---
+++ WpfApp1/WpfApp1/EditForm.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    
+    public partial class EditForm : Window
+    {
+        Window1 AdminPanel { get; set; }
+        public EditForm(Window1 adminpanel)
+        {
+            AdminPanel = adminpanel;
+            InitializeComponent();
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            AdminPanel.Show();
+        }
+    }
+}
