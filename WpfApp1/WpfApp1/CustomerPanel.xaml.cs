e4abbff8e12b9e93510b53269cfd111cae0e2347
Amir Salimi <amirhosein.srudsari@gmail.com>
2022-07-06 18:37:36 +0430

    Bookmarks Completed

--- WpfApp1/WpfApp1/CustomerPanel.xaml.cs
+++ WpfApp1/WpfApp1/CustomerPanel.xaml.cs
@@ -15,14 +15,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace WpfApp1
-{
-    
+{ 
+
     public partial class CustomerPanel : Window
     {
         public string EmailOfUser { get; set; }
         public string PassWordOfUser { get; set; }
         MainWindow main_window { get; set; }
-        public CustomerPanel(MainWindow window, string email , string pass)
+        public CustomerPanel(MainWindow window, string email, string pass)
         {
             main_window = window;
             EmailOfUser = email;
@@ -57,8 +57,86 @@ namespace WpfApp1
 
         private void BookmarkBtn_Click(object sender, RoutedEventArgs e)
         {
-            bookmarksGrid.Visibility = Visibility.Visible;
             MainGrid.Visibility = Visibility.Hidden;
+            string nameofbook = BookName.Text.ToString();
+            nameofbook = nameofbook.Replace("Name =", "").Replace(",","").Trim();
+            
+            bool exist = false;
+            string command;
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Coding\ApProject\Book-Store-\WpfApp1\Users.mdf;Integrated Security=True;Connect Timeout=30");
+
+            connection.Open();
+
+            command = "select * from UserTable";
+            SqlDataAdapter adapter = new SqlDataAdapter(command, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            int row = 0;
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                if (data.Rows[i][0].ToString() == EmailOfUser)
+                {
+                    exist = true;
+                    row = i;
+                }
+            }
+
+            if (!exist)
+            {
+                connection.Close();
+                MessageBoxResult messageBox = MessageBox.Show("There is no user with this email!");
+                return;
+            }
+
+            connection.Close();
+
+            string email;
+            string name;
+            string family;
+            string password;
+            string shoppinglist;
+            string buyedlist;
+            string bookmarked;
+            float wallet;
+
+            email = data.Rows[row][0].ToString();
+            name = data.Rows[row][1].ToString();
+            family = data.Rows[row][2].ToString();
+            password = data.Rows[row][3].ToString();
+            shoppinglist = data.Rows[row][4].ToString();
+            buyedlist = data.Rows[row][5].ToString();
+            bookmarked = data.Rows[row][6].ToString();
+            wallet = float.Parse(data.Rows[row][7].ToString());
+
+            if (bookmarked.Contains($"{nameofbook}"))
+            {
+                bookmarked = bookmarked.Replace($"{nameofbook}, ", "");
+                MessageBoxResult message = MessageBox.Show("You unbookmaarked the book");
+            }
+            else bookmarked += $"{nameofbook}, ";
+
+            try
+            {
+                connection.Open();
+
+                string DeleteCommand;
+                DeleteCommand = "delete from UserTable where Email = '" + EmailOfUser + "'";
+                SqlCommand DeleteRow = new SqlCommand(DeleteCommand, connection);
+                DeleteRow.ExecuteNonQuery();
+
+                command = "insert into UserTable values" +
+                        "('" + EmailOfUser.Trim() + "','" + name.Trim() + "' , '" + family.Trim() + "','" + password.Trim() + "','" + shoppinglist.Trim() + "','" + buyedlist + "','" + bookmarked + "','" + wallet + "')";
+
+                SqlCommand Command = new SqlCommand(command, connection);
+                Command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception Error)
+            {
+                connection.Close();
+                MessageBoxResult message = MessageBox.Show($"Bookmarking Book was unsuccessful!/nError descriiption : \n{Error.Message}");return;
+            }
         }
 
         private void CartBtn_Click(object sender, RoutedEventArgs e)
@@ -92,7 +170,8 @@ namespace WpfApp1
 
         private void bookmarksBack_Click(object sender, RoutedEventArgs e)
         {
-
+            bookmarksGrid.Visibility = Visibility.Hidden;
+            MainGrid.Visibility = Visibility.Visible;
         }
 
         private void cartBuyBtn_Click(object sender, RoutedEventArgs e)
@@ -174,7 +253,7 @@ namespace WpfApp1
         {
             if (!Check.NameCheck(searchBook.Text.ToString()))
             {
-                MessageBoxResult message = MessageBox.Show("Enter name");return;
+                MessageBoxResult message = MessageBox.Show("Enter name"); return;
             }
 
             string Name = searchBook.Text.ToString();
@@ -245,6 +324,7 @@ namespace WpfApp1
             BookName.Text = "Name = " + name;
             description.Text = "Description = " + bookdescription;
             Pricee.Text = "Price = " + price;
+
             if (isvip == true)
             {
                 VIPe.Text = "VIP";
@@ -264,18 +344,18 @@ namespace WpfApp1
 
         private void BackToSearchBookPaage(object sender, RoutedEventArgs e)
         {
-            ShowBook.Visibility=Visibility.Hidden;
+            ShowBook.Visibility = Visibility.Hidden;
             searchGrid.Visibility = Visibility.Visible;
         }
 
         private void AddToShoppingList(object sender, RoutedEventArgs e)
         {
             string nameofbook = searchBook.Text.ToString();
-            string nameofauthor  =searchAuthor.Text.ToString();
+            string nameofauthor = searchAuthor.Text.ToString();
 
-            if(nameofbook.Trim()!="" && nameofauthor.Trim() != "")
+            if (nameofbook.Trim() != "" && nameofauthor.Trim() != "")
             {
-                MessageBoxResult message = MessageBox.Show("Fill just one of the fields");return;
+                MessageBoxResult message = MessageBox.Show("Fill just one of the fields"); return;
             }
             bool exist = false;
             string command;
@@ -291,7 +371,7 @@ namespace WpfApp1
             int row = 0;
             for (int i = 0; i < data.Rows.Count; i++)
             {
-                if ( data.Rows[i][0].ToString().ToLower() == EmailOfUser)
+                if (data.Rows[i][0].ToString().ToLower() == EmailOfUser)
                 {
                     exist = true;
                     row = i;
@@ -334,11 +414,11 @@ namespace WpfApp1
             DataTable databook = new DataTable();
             adapter2.Fill(databook);
 
-            string price="";
+            string price = "";
 
             for (int i = 0; i < databook.Rows.Count; i++)
             {
-                if (nameofbook!=null && databook.Rows[i][0].ToString().ToLower() == nameofbook.ToLower())
+                if (nameofbook != null && databook.Rows[i][0].ToString().ToLower() == nameofbook.ToLower())
                 {
                     price = databook.Rows[i][3].ToString();
                     break;
@@ -351,9 +431,9 @@ namespace WpfApp1
             }
 
             connection.Close();
-            
-            
-           
+
+
+
             shoppinglist += $"{nameofbook} {price}, ";
 
             try
@@ -386,7 +466,7 @@ namespace WpfApp1
         {
             if (!Check.NameCheck(searchAuthor.Text.ToString()))
             {
-                MessageBoxResult message = MessageBox.Show("Enter name");return;
+                MessageBoxResult message = MessageBox.Show("Enter name"); return;
             }
 
             string Name = searchAuthor.Text.ToString();
@@ -471,6 +551,51 @@ namespace WpfApp1
             }
             searchGrid.Visibility = Visibility.Hidden;
             ShowBook.Visibility = Visibility.Visible;
+        }
+
+        private void BookIist(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Visibility = Visibility.Hidden;
+            bool exist = false;
+            string command;
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Coding\ApProject\Book-Store-\WpfApp1\Users.mdf;Integrated Security=True;Connect Timeout=30");
+
+            connection.Open();
+
+            command = "select * from UserTable";
+            SqlDataAdapter adapter = new SqlDataAdapter(command, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            int row = 0;
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                if (data.Rows[i][0].ToString().ToLower() == EmailOfUser)
+                {
+                    exist = true;
+                    row = i;
+                }
+            }
+
+            if (!exist)
+            {
+                connection.Close();
+                MessageBoxResult messageBox = MessageBox.Show("There is no user with this email!");
+                return;
+            }
+
+            connection.Close();
+            string bookmarked;
+            bookmarked = data.Rows[row][6].ToString();
+
+            //list
+            List<string> books = new List<string>();
+            string []arr = bookmarked.Split(',');
+
+            foreach(string nameofbookmarked in arr) { books.Add(nameofbookmarked.Trim()); }
+
+            YourListBox.ItemsSource = books;
+            bookmarksGrid.Visibility = Visibility.Visible;
         }
     }
 }
