8da927d6d5a19dc4adf73831dc469c0a6014692a
MSalehP <mohamadsalehpajand@gmail.com>
2022-07-09 08:51:36 +0430

    Last commit

--- WpfApp1/WpfApp1/AdminPannel.xaml.cs
+++ WpfApp1/WpfApp1/AdminPannel.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    
+
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
@@ -26,7 +26,7 @@ namespace WpfApp1
         string pass { get; set; }
         MainWindow Mainwindow { get; set; }
 
-        public Window1(string name , string pass , MainWindow window)
+        public Window1(string name, string pass, MainWindow window)
         {
             this.name = name;
             this.pass = pass;
@@ -36,7 +36,7 @@ namespace WpfApp1
 
         private void AddBook(object sender, RoutedEventArgs e)
         {
-            AddBookForm form = new AddBookForm( this);
+            AddBookForm form = new AddBookForm(this);
             this.Visibility = Visibility.Hidden;
             form.Show();
         }
@@ -105,7 +105,7 @@ namespace WpfApp1
 
         private void SearchBookByBookName(object sender, RoutedEventArgs e)
         {
-            if (nameofbook==null)
+            if (nameofbook == null)
             {
                 MessageBoxResult message = MessageBox.Show("Enter name");
             }
@@ -128,18 +128,18 @@ namespace WpfApp1
             string pdfpath;
             bool exist;
 
-            SQLmethodes.ReturnBookStats(0, Name, out name, out authorname, out year, out price, out bookdescription, out authorprofile, out isvip, out salenumber, out point, out bookimagepath, out vipfee, out timefordiscount, out discount,out  numberofpoints , out  pdfpath, out exist);
+            SQLmethodes.ReturnBookStats(0, Name, out name, out authorname, out year, out price, out bookdescription, out authorprofile, out isvip, out salenumber, out point, out bookimagepath, out vipfee, out timefordiscount, out discount, out numberofpoints, out pdfpath, out exist);
             if (!exist) return;
 
-            SearchBook searchBook = new SearchBook(name, price, year, authorname, authorprofile, bookdescription, isvip, salenumber, point, bookimagepath, vipfee, timefordiscount, discount, numberofpoints , pdfpath , this);
+            SearchBook searchBook = new SearchBook(name, price, year, authorname, authorprofile, bookdescription, isvip, salenumber, point, bookimagepath, vipfee, timefordiscount, discount, numberofpoints, pdfpath, this);
             this.Visibility = Visibility.Hidden;
             searchBook.Show();
         }
 
         private void SearchBookByAuthorName(object sender, RoutedEventArgs e)
         {
-            
-            if (authornamee==null)
+
+            if (authornamee == null)
             {
                 MessageBoxResult message = MessageBox.Show("Enter name");
             }
@@ -163,18 +163,15 @@ namespace WpfApp1
             string pdfpath;
             bool exist;
 
-            SQLmethodes.ReturnBookStats(1, Authorname, out name, out authorname, out year, out price, out bookdescription, out authorprofile, out isvip, out salenumber, out point, out bookimagepath, out vipfee, out timefordiscount, out discount, out numberofpoints , out pdfpath,out exist);
+            SQLmethodes.ReturnBookStats(1, Authorname, out name, out authorname, out year, out price, out bookdescription, out authorprofile, out isvip, out salenumber, out point, out bookimagepath, out vipfee, out timefordiscount, out discount, out numberofpoints, out pdfpath, out exist);
             if (!exist) return;
 
-            SearchBook searchBook = new SearchBook(name, price, year, authorname, authorprofile, bookdescription, isvip, salenumber, point, bookimagepath, vipfee, timefordiscount, discount,numberofpoints , pdfpath, this);
+            SearchBook searchBook = new SearchBook(name, price, year, authorname, authorprofile, bookdescription, isvip, salenumber, point, bookimagepath, vipfee, timefordiscount, discount, numberofpoints, pdfpath, this);
             this.Visibility = Visibility.Hidden;
             searchBook.Show();
         }
 
-        private void EmailSearchedButton(object sender, RoutedEventArgs e)
-        {
 
-        }
 
         private void SetVIPprice(object sender, RoutedEventArgs e)
         {
@@ -184,22 +181,22 @@ namespace WpfApp1
         private void SumbitmonthlyFee_Click(object sender, RoutedEventArgs e)
         {
             float monthlyFee;
-            if(!float.TryParse(MonthlyFee.Text.ToString() , out monthlyFee) || monthlyFee < 0)
+            if (!float.TryParse(MonthlyFee.Text.ToString(), out monthlyFee) || monthlyFee < 0)
             {
-                MessageBoxResult message = MessageBox.Show("Enter fee");return;
+                MessageBoxResult message = MessageBox.Show("Enter fee"); return;
             }
 
             float fee;
             bool exist;
 
             SQLmethodes.ReturnVIPfee(out exist, out fee);
-            if (fee==null)
+            if (fee == null)
             {
                 bool done;
-                SQLmethodes.AddToVIPfee(fee,out done);
+                SQLmethodes.AddToVIPfee(fee, out done);
                 if (!done)
                 {
-                    MessageBoxResult message = MessageBox.Show("VIP fee isn\'t added");return;
+                    MessageBoxResult message = MessageBox.Show("VIP fee isn\'t added"); return;
                 }
             }
             else
@@ -208,10 +205,10 @@ namespace WpfApp1
                 SQLmethodes.EditVIPfee(monthlyFee, out isok);
                 if (!isok)
                 {
-                    MessageBoxResult messageBox = MessageBox.Show("Edit isn\'t sumbited");return;
+                    MessageBoxResult messageBox = MessageBox.Show("Edit isn\'t sumbited"); return;
                 }
             }
-            MessageBoxResult message1 = MessageBox.Show("Fee sumbited"); 
+            MessageBoxResult message1 = MessageBox.Show("Fee sumbited");
 
             VIPSET.Visibility = Visibility.Hidden;
         }
@@ -233,6 +230,68 @@ namespace WpfApp1
         private void BackToAdminPannel(object sender, RoutedEventArgs e)
         {
             VIPSET.Visibility = Visibility.Hidden;
+        }
+
+        private void UserSearchedButton(object sender, RoutedEventArgs e)
+        {
+            SearchUserGrid.Visibility = Visibility.Visible;
+        }
+
+        private void UserSearchEmailBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = SQLmethodes.SQLconnectionToUsersTable();
+            string command = "select * from UserTable";
+            con.Open();
+            SqlDataAdapter adap = new SqlDataAdapter(command, con);
+            DataTable data = new DataTable();
+            adap.Fill(data);
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                if (data.Rows[i][0].ToString() == UsersearchEmail.Text)
+                {
+                    secondPage.Visibility = Visibility.Visible;
+                    firstPage.Visibility = Visibility.Hidden;
+                    UserEmail.Text = data.Rows[i][0].ToString();
+                    UserName.Text = data.Rows[i][1].ToString() + " " + data.Rows[i][2].ToString();
+                    USerVIPtime.Text = data.Rows[i][8].ToString();
+                    UserWallet.Text = data.Rows[i][7].ToString();
+                }
+            }
+            con.Close();
+        }
+
+        private void UserSerachNameBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = SQLmethodes.SQLconnectionToUsersTable();
+            string command = "select * from UserTable";
+            con.Open();
+            SqlDataAdapter adap = new SqlDataAdapter(command, con);
+            DataTable data = new DataTable();
+            adap.Fill(data);
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                if (data.Rows[i][1].ToString() == UserName.Text || data.Rows[i][2].ToString() == UserName.Text || UserName.Text == data.Rows[i][1] + " " + data.Rows[i][2])
+                {
+                    secondPage.Visibility = Visibility.Visible;
+                    firstPage.Visibility = Visibility.Hidden;
+                    UserEmail.Text = data.Rows[i][0].ToString();
+                    UserName.Text = data.Rows[i][1].ToString() + " " + data.Rows[i][2].ToString();
+                    USerVIPtime.Text = data.Rows[i][8].ToString();
+                    UserWallet.Text = data.Rows[i][7].ToString();
+                }
+            }
+            con.Close();
+        }
+
+        private void UserSearchBack_Click(object sender, RoutedEventArgs e)
+        {
+            SearchUserGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void UserPageBack_Click(object sender, RoutedEventArgs e)
+        {
+            firstPage.Visibility = Visibility.Visible;
+            secondPage.Visibility = Visibility.Hidden;
         }
     }
 }
